2c94ad101f49317850dbfe65f547fd4555d7f8c4
abhirawat623 <abhirawat623@gmail.com>
2023-07-14 00:06:50 +0530

    created rating selector logic

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { createAllMovies } from "./createMovies.js"
 
 const moviesParentContainer = document.getElementById('movies');
-
-
+const searcher = document.querySelector('.searcher');
+let searchedValue="";
+let arrOfFilteredMovie=[];
 const movieURL = "https://movies-app.prakashsakari.repl.co/api/movies";
 
 const getData = async (movieURL)=>{
@@ -22,3 +23,31 @@ console.log(movies)
 const createElement = (element)=> document.createElement(element);
 
 createAllMovies(movies,moviesParentContainer,createElement);
+
+function getFilteredData(){
+arrOfFilteredMovie = searchedValue?.length > 0 ? movies.filter(movie=>
+    //    (searchedValue.includesmovie.name.toLowerCase() ||
+    //    searchedValue === movie.director_name.toLowerCase() ||
+       movie.name.toLowerCase().includes(searchedValue) ||
+       movie.director_name.toLowerCase().includes(searchedValue)||
+       movie.writter_name.toLowerCase().split(",").includes(searchedValue) ||
+       movie.cast_name.toLowerCase().split(",").includes(searchedValue)) :  movies;
+       console.log(arrOfFilteredMovie);
+return arrOfFilteredMovie;
+
+}
+
+
+
+const handleOnSerch= (event)=>{
+    searchedValue= event.target.value.toLowerCase();
+ 
+ let arrOfSearchedMovie = getFilteredData();
+  console.log(arrOfSearchedMovie);
+  moviesParentContainer.innerHTML="";
+  createAllMovies(arrOfSearchedMovie,moviesParentContainer,createElement);
+
+}
+
+
+searcher.addEventListener("keyup",handleOnSerch)
